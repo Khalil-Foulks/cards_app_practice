bc57d07250eb72db0656796fd2a5fb90d9c1a67a
Khalil <khalilfoulks@gmail.com>
2023-08-01 12:07:29 -0400

    restructure app

---
+++ cards_practice/src/components/planets/Planets.js
@@ -0,0 +1,14 @@
+import React, {useState,} from "react";
+import PlanetDummyData from '../../dummyData';
+
+function Planets() {
+    const [planetData, setPlanetData] = useState(PlanetDummyData);
+
+    return (
+        <div className="Planets">
+            
+        </div>
+    )
+}
+
+export default Planets;
